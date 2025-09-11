6976238ee6e80cd61f804f5d0bcd4055f47d18fa
Tuan <PATuan262004@gmail.com>
2025-09-11 16:02:11 +0700

    status color

--- src/components/ContentItem/ContenItem.tsx
+++ src/components/ContentItem/ContenItem.tsx
@@ -1,4 +1,4 @@
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 interface Course {
     id: number;
     subject: string;
@@ -12,22 +12,50 @@ interface Course {
 interface ContentItemProps {
     course: Course;
 }
-function ContentItem({course}:ContentItemProps) {
-    const { containerItem, STT, Class, InfoClass, Time, Status, number } = styles;
-    return ( 
+function ContentItem({ course }: ContentItemProps) {
+    const StatusColor = (status: string) => {
+        switch (status) {
+            case "Chưa bắt đầu":
+                return styles.StatusNotstart;
+            case "Đang diễn ra":
+                return styles.StatusInprogress;
+            case "Đã hoàn thành":
+                return styles.StatusComplete;
+            case "Đã hủy":
+                return styles.StatusCancel;
+            default:
+                return "";
+        }
+    };
+    const { containerItem, STT, Class, InfoClass, Time, Status, number } =
+        styles;
+    return (
         <div className={containerItem}>
             <div className={STT}>{course.id}</div>
             <div className={Class}>{course.subject}</div>
             <div className={InfoClass}>
-                <div>Tên khóa học: <span>{course.courseName}</span></div>
-                <div>Tên Chương Trình: <span>{course.programName}</span></div>
-                <div>Sỹ số lớp: <span className={number}>{course.classSize}</span></div>
+                <div>
+                    Tên khóa học: <span>{course.courseName}</span>
+                </div>
+                <div>
+                    Tên Chương Trình: <span>{course.programName}</span>
+                </div>
+                <div>
+                    Sỹ số lớp:{" "}
+                    <span className={number}>{course.classSize}</span>
+                </div>
             </div>
             <div className={Time}>
-                <div>Ngày bắt đầu: <span>{course.startDate}</span></div>
-                <div>Ngày kết thúc: <span>{course.endDate}</span></div>
+                <div>
+                    Ngày bắt đầu: <span>{course.startDate}</span>
+                </div>
+                <div>
+                    Ngày kết thúc: <span>{course.endDate}</span>
+                </div>
             </div>
-            <div className={Status}>{course.status}</div>
+            <div className={`${Status} ${StatusColor(course.status)}`}>
+                {course.status}
+            </div>
         </div>
     );
 }
